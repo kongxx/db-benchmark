dd8fd41e1dec42aaf2cb376a43fa10c17c1256f4
kongxx <kongxx@gmail.com>
2014-12-08 21:25:22 +0800

    refactor insert test

--- src/test/java/InsertTest.java
+++ src/test/java/InsertTest.java
@@ -1,4 +1,3 @@
-package my;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -6,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.Timestamp;
 import java.sql.Types;
 import java.util.ArrayList;
@@ -15,21 +15,24 @@ import java.util.Formatter;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import org.apache.commons.dbutils.DbUtils;
-import org.apache.commons.dbutils.QueryRunner;
-import org.apache.commons.dbutils.ResultSetHandler;
-import org.apache.commons.lang.math.RandomUtils;
-
 public class InsertTest {
 
 	private static Logger logger = Logger.getLogger(InsertTest.class.getName());
 
-	private static final String TABLE_NAME = "BENCHMARK_COL";
+	private static final String DB_DRIVER = "com.ibm.db2.jcc.DB2Driver";
+	private static final String DB_URL = "jdbc:db2://9.111.251.152:50000/padb";
+	private static final String DB_USERNAME = "db2inst";
+	private static final String DB_PASSWORD = "Letmein";
+
+	private static Random random = new Random(10000);
+
+	private static final String TABLE_NAME = "BENCHMARK2";
 
 	private int batchSize;
 
@@ -39,11 +42,15 @@ public class InsertTest {
 
 	public static void main(String[] args) throws Exception {
 		printHeader();
-		new InsertTest(1000, 1).run();
-		new InsertTest(1000, 1).run();
-		new InsertTest(2000, 1).run();
-		new InsertTest(5000, 1).run();
-		new InsertTest(10000, 1).run();
+
+		int[] concurrentList = new int[]{1, 5, 10, 20};
+		int[] batchSizeList = new int[] {1000, 2000, 5000, 10000, 20000};
+		for (int concurrent : concurrentList) {
+			for (int batchSize : batchSizeList) {
+				new InsertTest(batchSize, concurrent).run(true);
+			}
+			Thread.sleep(10000);
+		}
 	}
 
 	public InsertTest(final int batchSize, final int concurrent) throws Exception {
@@ -58,7 +65,7 @@ public class InsertTest {
 		this.sampling = sampling;
 	}
 
-	public void run() throws Exception {
+	public void run(boolean printResult) throws Exception {
 		final List<Long> results = Collections.synchronizedList(new ArrayList<Long>());
 		final CountDownLatch startGate = new CountDownLatch(concurrent);
 		final CountDownLatch endGate = new CountDownLatch(concurrent);
@@ -83,7 +90,9 @@ public class InsertTest {
 
 		Collections.sort(results);
 
-		printResult(batchSize, concurrent, results);
+		if (printResult) {
+			printResult(batchSize, concurrent, results);
+		}
 	}
 
 	public long execute() throws Exception {
@@ -100,10 +109,11 @@ public class InsertTest {
 			return stop - start;
 		} catch(Exception ex) {
 			logger.log(Level.SEVERE, null, ex);
-			DbUtils.rollbackAndCloseQuietly(conn);
+			conn.rollback();
+			conn.close();
 			throw ex;
 		} finally {
-			DbUtils.closeQuietly(conn);
+			conn.close();
 		}
 	}
 
@@ -130,7 +140,8 @@ public class InsertTest {
 			if (null != ex.getNextException()) {
 				logger.log(Level.SEVERE, null, ex.getNextException());
 			}
-			DbUtils.rollback(conn);
+			conn.rollback();
+			throw ex;
 		}
 	}
 
@@ -156,23 +167,13 @@ public class InsertTest {
 	}
 
 	private Map<String, Integer> queryTableColumns(Connection conn) throws Exception {
-		Map<String, Integer> columns = null;
-		try {
-			QueryRunner queryRunner = new QueryRunner();
-			String sql = "SELECT * FROM " + TABLE_NAME + " WHERE 1=0";
-			columns = queryRunner.query(conn, sql, new ResultSetHandler<Map<String, Integer>>() {
-				@Override
-				public Map<String, Integer> handle(ResultSet rs) throws SQLException {
-					Map<String, Integer> columns = new LinkedHashMap<String, Integer>();
-					ResultSetMetaData rsmd = rs.getMetaData();
-					for (int i = 1; i <= rsmd.getColumnCount(); i++) {
-						columns.put(rsmd.getColumnName(i), rsmd.getColumnType(i));
-					}
-					return columns;
-				}
-			});
-		} finally {
-			DbUtils.closeQuietly(conn);
+		Map<String, Integer> columns = new LinkedHashMap<String, Integer>();
+		String sql = "SELECT * FROM " + TABLE_NAME + " WHERE 1=0";
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery(sql);
+		ResultSetMetaData rsmd = rs.getMetaData();
+		for (int i = 1; i <= rsmd.getColumnCount(); i++) {
+			columns.put(rsmd.getColumnName(i), rsmd.getColumnType(i));
 		}
 		return columns;
 	}
@@ -189,7 +190,7 @@ public class InsertTest {
 			case Types.TINYINT:
 			case Types.SMALLINT:
 			case Types.INTEGER:
-				obj = RandomUtils.nextInt(10000);
+				obj = random.nextInt(10000);
 				break;
 			case Types.DATE:
 				obj = Calendar.getInstance().getTime();
@@ -198,21 +199,15 @@ public class InsertTest {
 				obj = new Timestamp(System.currentTimeMillis());
 				break;
 			default:
-				obj = String.valueOf(RandomUtils.nextInt(10000));
+				obj = String.valueOf(random.nextInt(10000));
 				break;
 		}
 		return obj;
 	}
 
 	private Connection getConnection() throws Exception {
-		Class.forName("com.ibm.db2.jcc.DB2Driver");
-		String url = "jdbc:db2://padev3:50000/padb";
-		Connection conn = DriverManager.getConnection(url, "db2inst", "Letmein");
-
-//		Class.forName("com.vertica.jdbc.Driver");
-//		String url = "jdbc:vertica://9.111.251.152:5433/padb";
-//		Connection conn = DriverManager.getConnection(url, "pauser", "papassword");
-
+		Class.forName(DB_DRIVER);
+		Connection conn = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
 		conn.setAutoCommit(false);
 		return conn;
 	}
@@ -220,7 +215,7 @@ public class InsertTest {
 	private static void printHeader() {
 		StringBuilder sb = new StringBuilder();
 		sb.append("\n");
-		sb.append(new Formatter().format("%12s|%12s|%10s|%10s|%10s", "BATCH_SIZE", "CONCURRENT", "AVG (r/s)", "MIN (r/s)", "MAX (r/s)"));
+		sb.append(new Formatter().format("%15s|%15s|%15s|%15s|%15s", "BATCH_SIZE", "CONCURRENT", "AVG (r/s)", "MIN (r/s)", "MAX (r/s)"));
 		System.out.println(sb.toString());
 	}
 
@@ -230,7 +225,7 @@ public class InsertTest {
 			total += result;
 		}
 		StringBuilder sb = new StringBuilder();
-		sb.append(new Formatter().format("%12s|%12s|%10s|%10s|%10s", batch, concurrent, (total/results.size()), results.get(0), results.get(results.size() - 1)));
+		sb.append(new Formatter().format("%15s|%15s|%15s|%15s|%15s", batch, concurrent, (total/results.size()), results.get(0), results.get(results.size() - 1)));
 		System.out.println(sb.toString());
 	}
 }
